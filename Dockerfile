a315864fa53f9090b0dfaeaf94fa48ce27ac82c9
elblasco <alessio.blascovich@studenti.unitn.it>
2026-05-11 16:23:05 +0200

    fix: docker build

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM maven:3.9.9-eclipse-temurin-17 AS builder
 WORKDIR /app
 
 COPY pom.xml .
-RUN mvn -B -q -e dependency:go-offline
+RUN mvn -B -q -e -DskipTests dependency:go-offline
 COPY src ./src
 RUN mvn clean package -DskipTests
 
@@ -15,7 +15,7 @@ RUN chmod +x /usr/local/bin/init-db.sh
 VOLUME ["/data"]
 
 RUN rm -rf /usr/local/tomcat/webapps/*
-COPY --from=builder /app/target/Mnemosyne-1.0-SNAPSHOT.war /usr/local/tomcat/webapps/mnemosyne.war
+COPY --from=builder /app/target/mnemosyne-1.0-SNAPSHOT.war /usr/local/tomcat/webapps/mnemosyne.war
 COPY server-keystore.p12 /usr/local/tomcat/conf/keystore.p12
 RUN sed -i 's|</Service>|\
 <Connector port="8443" protocol="org.apache.coyote.http11.Http11NioProtocol" \
---
+++ src/main/java/it/unitn/apcm/blasco/mnemosyne/utils/Utils.java
@@ -0,0 +1,32 @@
+package it.unitn.apcm.blasco.mnemosyne.Utils;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.util.encoders.Hex;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.Security;
+
+public class Utils {
+    static {
+        Security.addProvider(new BouncyCastleProvider());
+    }
+    public static final String DB_URL = "jdbc:sqlite:/data/mnemosyne.db";
+
+    /**
+     * Hashes a string with SHA-256 using Bouncy Castle.
+     *
+     * @param password The string to hash.
+     * @param salt     Salt to append.
+     * @return         Hexadecimal representation of the SHA-256 hash.
+     * @throws NoSuchAlgorithmException If "SHA-256" is unavailable.
+     * @throws NoSuchProviderException  If Bouncy Castle provider is not registered.
+     */
+    static public String hashPassword(String password, String salt) throws NoSuchAlgorithmException, NoSuchProviderException {
+        MessageDigest digest = MessageDigest.getInstance("SHA-256", "BC");
+        byte[] hashBytes = digest.digest((password + salt).getBytes(StandardCharsets.UTF_8));
+        return Hex.toHexString(hashBytes);
+    }
+}
